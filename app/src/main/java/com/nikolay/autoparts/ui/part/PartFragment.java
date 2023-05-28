e2c150b46e298a1bfaa68cd4d6a625349543e159
Nikolay Nedyalkov <n.nedialkov@stenik.bg>
2023-05-28 20:29:29 +0300

    Modification

---
+++ app/src/main/java/com/nikolay/autoparts/ui/part/PartFragment.java
@@ -0,0 +1,38 @@
+package com.nikolay.autoparts.ui.part;
+
+import androidx.lifecycle.ViewModelProvider;
+
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.nikolay.autoparts.R;
+
+public class PartFragment extends Fragment {
+
+    private PartViewModel mViewModel;
+
+    public static PartFragment newInstance() {
+        return new PartFragment();
+    }
+
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,
+                             @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_part, container, false);
+    }
+
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+        mViewModel = new ViewModelProvider(this).get(PartViewModel.class);
+        // TODO: Use the ViewModel
+    }
+
+}
---
+++ app/src/main/java/com/nikolay/autoparts/ui/part/PartViewModel.java
@@ -0,0 +1,7 @@
+package com.nikolay.autoparts.ui.part;
+
+import androidx.lifecycle.ViewModel;
+
+public class PartViewModel extends ViewModel {
+    // TODO: Implement the ViewModel
+}
